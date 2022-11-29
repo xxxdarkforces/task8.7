0045c7723fc24a9b94f2025e10b788fc8998d391
xxxdarkforces <xxx_darkforces@mail.ru>
2022-11-29 22:18:31 +0700

    edit

--- script.js
+++ script.js
@@ -1,113 +1,90 @@
 function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-/*
-function nanMin() {
-    if (isNaN(minValue)) {
-        return minValue = -999;
-    } else if (minValue <= -1000) {
-        return minValue = -999;
-    } else {
-        return minValue;
-    }
-}*/
-function nanMax() {
-    if (isNaN(maxValue)) {
-        return maxValue = 999;
-    } else if (maxValue >= 1000) {
-        return maxValue = 999;
-    } else {
-        return maxValue;
-    }
+
+function limitMinValue() {
+    minValue = (minValue <= -1000 || minValue >= 1000) || (isNaN(minValue)) ? -999 : minValue;
+    return minValue;
 }
 
-
-function minMinValue() {
-    minValue =  (minValueq <= -1000) ? "444" : "555";
+function limitMaxValue() {
+    maxValue = (maxValue >= 1000 || maxValue <= -1000) || (isNaN(maxValue)) ? 999 : maxValue;
+    return maxValue;
 }
-let minValue = -999;
-let minValueq = parseInt(prompt('Минимальное знание числа для игры', '1'));
-let maxValue = parseInt(prompt('Максимальное знание числа для игры', '100'));
 
-minValue = minMinValue(minValueq);
-maxValue = nanMax(maxValue);
+function restartGameRun() {
+    alert("Вы указали минимальное число больше или равное максимальному, пожалуйста укажите верный диапозон! Начнём все занова...");
+    startGame();
+    return
+}
 
-let arrСondition = [`Загадайте любое целое число от ${minValue} до ${maxValue}, я буду его отгадывать`, `Загадайте любое целое число от ${minValue} до ${maxValue}, я быстро справлюсь`, `Придумайте целое число от ${minValue} до ${maxValue}, я его отгадаю`];
-condition = arrСondition[getRandomInRange(0, 2)];
-alert(condition);
-
-let answerNumber = Math.floor((minValue + maxValue) / 2);
-let orderNumber = 1;
-let gameRun = true;
-
-let orderNumberField = document.getElementById('orderNumberField');
-let answerField = document.getElementById('answerField');
-
-orderNumberField.innerText = orderNumber;
-answerField.innerText = `Вы загадали число ${answerNumber}?`;
-
-document.getElementById('btnRetry').addEventListener('click', function () {
-    minValue = parseInt(prompt('Минимальное знание числа для игры', '1'));
-    maxValue = parseInt(prompt('Максимальное знание числа для игры', '100'));
-
-    arrСondition = [`Загадайте любое целое число от ${minValue} до ${maxValue}, я буду его отгадывать`, `Загадайте любое целое число от ${minValue} до ${maxValue}, я быстро справлюсь`, `Придумайте целое число от ${minValue} до ${maxValue}, я его отгадаю`];
+function startGame() {
+    let gameRun = true;
+    minValue = parseInt(prompt('Укажите минимальное знание числа для игры (минимум -999)', '1'));
+    maxValue = parseInt(prompt('Укажите максимальное знание числа для игры (максимум 999)', '100'));
+    minValue = limitMinValue(minValue);
+    maxValue = limitMaxValue(maxValue);
+    gameRun = minValue <= maxValue ? true : restartGameRun();
+    let arrСondition = [`Загадайте любое целое число от ${minValue} до ${maxValue}, я буду его отгадывать`, `Загадайте любое целое число от ${minValue} до ${maxValue}, я быстро справлюсь`, `Придумайте целое число от ${minValue} до ${maxValue}, я его отгадаю`];
     condition = arrСondition[getRandomInRange(0, 2)];
     alert(condition);
 
-    answerNumber = Math.floor((minValue + maxValue) / 2);
-    orderNumber = 1;
-    gameRun = true;
-
+    let answerNumber = Math.floor((minValue + maxValue) / 2);
     arrQestion = [`Вы загадали число ${answerNumber}?`, `Наверное, это число ${answerNumber}?`, `Возможно это число ${answerNumber}?`];
     qestion = arrQestion[getRandomInRange(0, 2)];
-
+    let orderNumber = 1;
+    let orderNumberField = document.getElementById('orderNumberField');
+    let answerField = document.getElementById('answerField');
     orderNumberField.innerText = orderNumber;
     answerField.innerText = qestion;
-})
+    const ARR_ANSWER_PHRASE = [`Вы загадали неправильное число!\n\u{1F914}`, `Я сдаюсь..\n\u{1F92F}`, `Что то вы меня обманываете\n\u{1F635}`];
+    const ARR_EQUAL = [`Я всегда угадываю\n\u{1F60E}`, `Это было легко\n\u{1F60E}`, `Как все просто\n\u{1F60E}`];
 
-document.getElementById('btnOver').addEventListener('click', function () {
-    if (gameRun) {
-        if (minValue === maxValue) {
-            const arrAnswerPhrase = [`Вы загадали неправильное число!\n\u{1F914}`, `Я сдаюсь..\n\u{1F92F}`, `Что то вы меня обманываете\n\u{1F635}`];
-            answerPhrase = arrAnswerPhrase[getRandomInRange(0, 2)];
-            answerField.innerText = answerPhrase;
-            gameRun = false;
-        } else {
-            minValue = answerNumber + 1;
-            answerNumber = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
-            orderNumberField.innerText = orderNumber;
-            const arrAnswerFieldQ = [`Вы загадали число ${answerNumber}?`, `Возможно это число ${answerNumber}?`, `Наверное, это число ${answerNumber}?`];
-            answerFieldQ = arrAnswerFieldQ[getRandomInRange(0, 2)];
-            answerField.innerText = answerFieldQ;
+    document.getElementById('btnOver').addEventListener('click', function () {
+        if (gameRun) {
+            if (minValue === maxValue || minValue > maxValue) {
+                answerPhrase = ARR_ANSWER_PHRASE[getRandomInRange(0, 2)];
+                answerField.innerText = answerPhrase;
+                gameRun = false;
+            } else {
+                minValue = answerNumber + 1;
+                answerNumber = Math.floor((minValue + maxValue) / 2);
+                orderNumber++;
+                orderNumberField.innerText = orderNumber;
+                let arrAnswerFieldQ = [`Вы загадали число ${answerNumber}?`, `Возможно это число ${answerNumber}?`, `Наверное, это число ${answerNumber}?`];
+                answerFieldQ = arrAnswerFieldQ[getRandomInRange(0, 2)];
+                answerField.innerText = answerFieldQ;
+            }
         }
-    }
-})
+    })
 
-document.getElementById('btnLess').addEventListener('click', function () {
-    if (gameRun) {
-        if (minValue === maxValue || maxValue < minValue || minValue > answerNumber - 1) {
-            const arrAnswerPhrase = [`Вы загадали неправильное число!\n\u{1F914}`, `Я сдаюсь..\n\u{1F92F}`, `Что то вы меня обманываете\n\u{1F635}`];
-            answerPhrase = arrAnswerPhrase[getRandomInRange(0, 2)];
-            answerField.innerText = answerPhrase;
-            gameRun = false;
-        } else {
-            maxValue = answerNumber - 1;
-            answerNumber = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
-            orderNumberField.innerText = orderNumber;
-            const arrAnswerFieldQ = [`Вы загадали число ${answerNumber}?`, `Возможно это число ${answerNumber}?`, `Наверное, это число ${answerNumber}?`];
-            answerFieldQ = arrAnswerFieldQ[getRandomInRange(0, 2)];
-            answerField.innerText = answerFieldQ;
+    document.getElementById('btnLess').addEventListener('click', function () {
+        if (gameRun) {
+            if (minValue === maxValue || maxValue < minValue || minValue > answerNumber - 1) {
+                answerPhrase = ARR_ANSWER_PHRASE[getRandomInRange(0, 2)];
+                answerField.innerText = answerPhrase;
+                gameRun = false;
+            } else {
+                maxValue = answerNumber - 1;
+                answerNumber = Math.floor((minValue + maxValue) / 2);
+                orderNumber++;
+                orderNumberField.innerText = orderNumber;
+                let arrAnswerFieldQ = [`Вы загадали число ${answerNumber}?`, `Возможно это число ${answerNumber}?`, `Наверное, это число ${answerNumber}?`];
+                answerFieldQ = arrAnswerFieldQ[getRandomInRange(0, 2)];
+                answerField.innerText = answerFieldQ;
+            }
         }
-    }
-})
+    })
 
-document.getElementById('btnEqual').addEventListener('click', function () {
-    if (gameRun) {
-        const arrEqual = [`Я всегда угадываю\n\u{1F60E}`, `Это было легко\n\u{1F60E}`, `Как все просто\n\u{1F60E}`];
-        equal = arrEqual[getRandomInRange(0, 2)];
-        answerField.innerText = equal;
-        gameRun = false;
-    }
-})
+    document.getElementById('btnEqual').addEventListener('click', function () {
+        if (gameRun) {
+            equal = ARR_EQUAL[getRandomInRange(0, 2)];
+            answerField.innerText = equal;
+            gameRun = false;
+        }
+    })
+    document.getElementById('btnRetry').addEventListener('click', startGame);
+    return
+}
+
+startGame();
